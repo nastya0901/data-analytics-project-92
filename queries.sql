58786545e493c640a9fb3d3728c5a56607b8444e
nastya0901 <133377810+nastya0901@users.noreply.github.com>
2023-06-09 23:08:51 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -33,6 +33,39 @@ left join products p
 on s.product_id = p.product_id
 group by 2,3,1) as t
 order by num, name
-;
+;*/
+
+/*select 
+case when age between 10 and 15 then '10-15' 
+     when age between 16 and 25 then '16-25'
+     when age between 26 and 40 then '26-40'else '40+' end as age_category,
+    count (*) as age from customers
+   group by age_category
+   order by age_category;*/
+
+/*select  to_char(sale_date, 'YYYY-MM') as date, count ( distinct name)as total_customer, sum(price*quantity)as income from sales s
+left join products p
+on s.product_id = p.product_id
+group by date;*/
+
+
+
+select customer, sale_date, seller from (
+select s.customer_id, CONCAT (c.first_name,' ',c.last_name) as customer,
+sale_date, CONCAT (e.first_name,' ',e.last_name) as seller, p.price, s.sales_id,
+row_number() over (partition by s.customer_id order by sale_date, sales_id) as salenumber
+from sales s
+left join employees e  
+on s.sales_person_id = e.employee_id
+left join products p
+on s.product_id = p.product_id
+left join customers c
+on s.customer_id=c.customer_id
+) s 
+where salenumber = 1 and price = 0
+order by customer_id
+
+
+
 
 
